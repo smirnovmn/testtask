7cfcd1abee4c0fea19a2756c5f0c53e8ec5e7bbd
Михаил Смирнов <rucka.1@yandex.ru>
2025-08-20 18:29:37 +0300

    minor fix

--- src/app/core/db.py
+++ src/app/core/db.py
@@ -1,31 +1,3 @@
-# from sqlalchemy import Column, Integer
-# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-# from sqlalchemy.orm import declarative_base, declared_attr, sessionmaker
-
-# from app.core.config import settings
-
-
-# class PreBase:
-
-#     @declared_attr
-#     def __tablename__(cls):
-#         return cls.__name__.lower()
-
-#     id = Column(Integer, primary_key=True)
-
-
-# Base = declarative_base(cls=PreBase)
-
-# engine = create_async_engine(settings.database_url)
-
-# AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
-
-
-# async def get_async_session():
-#     async with AsyncSessionLocal() as async_session:
-#         yield async_session
-
-
 from sqlalchemy import Column, Integer
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, declared_attr, sessionmaker
@@ -43,10 +15,9 @@ class PreBase:
 
 Base = declarative_base(cls=PreBase)
 
-# Для PostgreSQL обязательно postgresql+asyncpg://...
 engine = create_async_engine(
     settings.database_url,
-    echo=True,  # Лог запросов в консоль (выключи на проде)
+    echo=True,
     future=True
 )
 
